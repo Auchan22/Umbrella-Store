880fa14d0b465a17196bb1031350fc9dec9cbf9d
Auchan22 <agustinsurila@gmail.com>
2022-01-04 22:59:09 -0300

    Pagina principal de la app

---
+++ src/Pages/PageHome.js
@@ -0,0 +1,15 @@
+import Layout from "../components/Layout"
+import ScrollBar from "../components/ScrollBar"
+import "./styles.css"
+
+const PageHome = () => {
+    return (
+        <>
+            <Layout>
+                <ScrollBar />
+            </Layout>
+        </>
+    )
+}
+
+export default PageHome
