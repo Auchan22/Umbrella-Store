693a240e1f951fe8ef1e950315a39a859e332161
Auchan22 <agustinsurila@gmail.com>
2022-01-06 17:02:32 -0300

    Componente dedicado al footer de la pagina

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import "./styles/footer.css"
+
+const Footer = () => {
+    return (
+        <footer>
+            footer
+            <hr/>
+            <h5>Agustin Surila Soto</h5>
+        </footer>
+    )
+}
+
+export default Footer
