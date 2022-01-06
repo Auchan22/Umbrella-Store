0f46b4a8bac7c5f04de1efe4ad7ae5c15c3d9262
Auchan22 <agustinsurila@gmail.com>
2022-01-06 16:59:54 -0300

    Se finalizo el componente referido a cada card de los productos, asi poder mostrar los datos de manera dinamica

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,7 +1,28 @@
-const ProductCard = () => {
+import { useState } from "react"
+import "./styles/productCard.css"
+
+const ProductCard = ({data}) => {
+
+    const [info, setInfo] = useState(data)
+    // console.log(info)
+
     return (
-        <div>
-            
+        <div className="container-card">
+            {/* <img src={info.productImg[0]} alt={info.productName} className="card-img" /> */}
+            <div className="card-img"></div>
+            <div className="product-info">
+                <div className="info-header">
+                    <h4 className="product-title">{info.productName}</h4>
+                    <p className="product-price">${info.price}</p>
+                </div>
+                <div className="info-body">
+                <p className="product-description">Descripcion</p>
+                </div>
+            </div>
+            <span className="category">
+                <p>{info.category[0].toUpperCase() + info.category.slice(1)}</p>
+            </span>
+            <button className="buy"><i className="fas fa-cart-plus"></i></button>
         </div>
     )
 }
