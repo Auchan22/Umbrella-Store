e6890d3b74adf1056bea6f99ac6727655278212d
Auchan22 <agustinsurila@gmail.com>
2022-01-07 16:43:57 -0300

    Add comments

--- src/Pages/PageHome.js
+++ src/Pages/PageHome.js
@@ -1,18 +1,26 @@
-
-import Layout from "../components/Layout"
-import ProductList from "../components/ProductList"
-import ScrollBar from "../components/ScrollBar"
-import "./styles.css"
+import { useState } from "react";
+import Layout from "../components/Layout";
+import ProductList from "../components/ProductList";
+import ScrollBar from "../components/ScrollBar";
+import "./styles.css";
 
 const PageHome = () => {
-    return (
-        <>
-            <Layout>
-                <ScrollBar />
-                <ProductList />
-            </Layout>
-        </>
-    )
-}
+  const [category, setCategory] = useState("Todos");
 
-export default PageHome
+  // Funcion que se encarga de tomar el filtro elegido en ScrollBar, y pasarlo a la lista de productos, para luego poder renderizar los productos acorde a ese filtro
+  const handleFiltro = (filter) => {
+    setCategory(filter);
+  };
+  // console.log(category);
+
+  return (
+    <>
+      <Layout>
+        <ScrollBar handleFiltro={handleFiltro} />
+        <ProductList category={category} />
+      </Layout>
+    </>
+  );
+};
+
+export default PageHome;
